518d285df9106f98e15edf9339e86851ad36ab1e
shravanc <shravan007.c@gmail.com>
2019-11-30 13:47:41 +0000

    seed changes

--- vod_platform/db/seeds.rb
+++ vod_platform/db/seeds.rb
@@ -1,4 +1,5 @@
 
+=begin
 provider = Provider.where(title: 'netflix').first_or_create
 
 tenant = Tenant.where(title: 'tenant_1', subdomain: 'netflix', provider_id: provider.id).first_or_create
@@ -6,3 +7,12 @@ tenant = Tenant.where(title: 'tenant_1', subdomain: 'netflix', provider_id: prov
 app = App.where(title: "android", tenant_id: tenant.id ).first_or_create
 
 services = Service.where(system: [{"service": "list_system", "host": "http://localhost:3001/items"}, {"service": "user_system", "host": "http://localhost:3002/users" }]).first_or_Create
+=end
+
+provider = Provider.where(title: 'amazon').first_or_create
+tenant   = Tenant.where(title: 'amazon', subdomin: 'amazon', provider_id: provider.id).first_or_create
+app      = App.where(title: "android", tenant_id: tenant.id).first_or_create
+
+provider = Provider.where(title: 'netflix').first_or_create
+tenant   = Tenant.where(title: 'netflix', subdomin: 'netflix', provider_id: provider.id).first_or_create
+app      = App.where(title: "android", tenant_id: tenant.id).first_or_create
